4eff1f2bd4b0e92808770111cafe22dd67fff4da
KermitPurple96 <103221169+KermitPurple96@users.noreply.github.com>
2022-10-04 16:22:42 +0200

    Update access_point.sh

--- access_point.sh
+++ access_point.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 AP=$(/usr/sbin/route | grep default | awk '{print $2}')
 if [[ "$AP" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
   echo "%{F#2495e7}泌 %{F#ffffff}$(/usr/sbin/route | grep default | awk '{print $2}')%{u-}"
