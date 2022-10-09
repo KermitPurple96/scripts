4658d05d0b76107b4f2e959ccbb5f1b2340eca51
KermitPurple96 <103221169+KermitPurple96@users.noreply.github.com>
2022-10-09 21:58:54 +0200

    Delete access_point.sh

--- access_point.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-AP=$(/usr/sbin/route | grep default | awk '{print $2}')
-if [[ "$AP" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-  echo "%{F#2495e7}泌 %{F#ffffff}$(/usr/sbin/route | grep default | awk '{print $2}')%{u-}"
-else 
-  echo "%{F#2495e7}泌 %{F#ffffff}Disconnected"
-fi
