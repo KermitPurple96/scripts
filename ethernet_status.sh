18656a593ccfa8bfbf0ea64c29eefd23fd52fd0c
KermitPurple96 <103221169+KermitPurple96@users.noreply.github.com>
2022-10-09 22:06:51 +0200

    Delete ethernet_status.sh

--- ethernet_status.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
- 
-IP=$(/usr/sbin/ifconfig | grep ens33 -A1 | grep inet | awk '{print $2}')
-if [[ "$IP" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-  echo "%{F#2495e7} %{F#ffffff}$(/usr/sbin/ifconfig | grep ens33 -A1 | grep inet | awk '{print $2}')%{u-}"
-else 
-  echo "%{F#2495e7} %{F#ffffff}Disconnected"
-fi
