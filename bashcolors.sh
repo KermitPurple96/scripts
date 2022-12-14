aec2071fbc40017b631d08a6b4adfe8b166d6ae8
KermitPurple96 <103221169+KermitPurple96@users.noreply.github.com>
2022-12-14 13:40:33 +0100

    Create bashcolors.sh

---
+++ bashcolors.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+ 
+green="\e[0;32m\033[1m"
+endcolor="\033[0m\e[0m"
+red="\e[0;31m\033[1m"
+blue="\e[0;34m\033[1m"
+yellow="\e[0;33m\033[1m"
+purple="\e[0;35m\033[1m"
+turquoise="\e[0;36m\033[1m"
+gray="\e[0;37m\033[1m"
+negro="\e[0;30m\033[1m"
+fondonegro="\e[0;40m\033[1m"
+fondoverde="\e[0;42m\033[1m"
+fondoamarillo="\e[0;43m\033[1m"
+fondoazul="\e[0;44m\033[1m"
+fondopurple="\e[0;46m\033[1m"
+fondogris="\e[0;47m\033[1m"
+ 
+ 
+ 
+tput civis
+trap ctrl_c INT
+ 
+function ctrl_c(){
+  echo -en "\n${red}[!]${end} Saliendo... \n"
+  tput cnorm; exit 1
+}
