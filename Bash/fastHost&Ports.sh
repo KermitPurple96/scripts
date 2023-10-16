e7151ee1e5916132046cf8450a3d470a2dc1f4ff
KermitPurple96 <103221169+KermitPurple96@users.noreply.github.com>
2023-10-16 13:00:41 +0200

    Update fastHost&Ports.sh

--- Bash/fastHost&Ports.sh
+++ Bash/fastHost&Ports.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-for port in 21 22 25 80 88 443 445 8080 8081; do
+for port in 21 22 23 25 53 80 88 110 123 161 443 445 636 3128 3306 3389 8080 8081 5985; do
   for i in $(seq 1 254); do 
     proxychains timeout 1 bash -c "echo '' > /dev/tcp/10.241.251.$i/$port" 2>/dev/null && echo "[+] port $port OPEN on host 10.241.251.$i" &
   done; wait
