9d09795ec7ad86debd9dc066d40e7a93717e1302
KermitPurple96 <103221169+KermitPurple96@users.noreply.github.com>
2025-01-20 20:10:58 +0100

    Update parseUrls.sh

--- Bash/parseUrls.sh
+++ Bash/parseUrls.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# parses URLs extracted from hakrawler removing unwanted and duplicate domains
+# parseUrls.sh urls.txt domain
+# result example:
+# http://blog.inlanefreight.local/?page_id=2
+# http://blog.inlanefreight.local/?page_id=2#respond
+# http://blog.inlanefreight.local/wp-admin/admin-ajax.php?action=wpdAddSubscription
+# http://blog.inlanefreight.local/wp-admin/js/password-strength-meter.min.js?ver=5.8
+
 # Verificar que se pasaron los argumentos necesarios
 if [ -z "$1" ] || [ -z "$2" ]; then
     echo "Uso: $0 archivo_de_urls.txt dominio_a_filtrar"
@@ -15,4 +23,4 @@ while IFS= read -r line; do
     if [[ "$domain" == *"$2"* ]]; then
         echo "$line"
     fi
-done < "$1" | sort -u | sponge "$1"
+done < "$1" | sort -u | uniq | sponge "$1"
