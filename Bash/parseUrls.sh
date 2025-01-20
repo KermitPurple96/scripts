5f8e3286e0d5b825918eb20756c995c4c12c9fe5
KermitPurple96 <103221169+KermitPurple96@users.noreply.github.com>
2025-01-20 20:11:22 +0100

    Update parseUrls.sh

--- Bash/parseUrls.sh
+++ Bash/parseUrls.sh
@@ -8,18 +8,14 @@
 # http://blog.inlanefreight.local/wp-admin/admin-ajax.php?action=wpdAddSubscription
 # http://blog.inlanefreight.local/wp-admin/js/password-strength-meter.min.js?ver=5.8
 
-# Verificar que se pasaron los argumentos necesarios
 if [ -z "$1" ] || [ -z "$2" ]; then
     echo "Uso: $0 archivo_de_urls.txt dominio_a_filtrar"
     exit 1
 fi
 
-# Filtrar URLs y extraer el dominio
 while IFS= read -r line; do
-    # Extraer el primer dominio entre // y /
     domain=$(echo "$line" | grep -oP '://([a-zA-Z0-9.-]+)' | head -n 1 | cut -d'/' -f3)
     
-    # Comprobar si el dominio contiene el valor pasado como argumento
     if [[ "$domain" == *"$2"* ]]; then
         echo "$line"
     fi
